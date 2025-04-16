abd5826705f299b5ad781400e329c9d023b84c4c
Vinay Kaundinya <vinaykaundinya@Vinays-Work-Air.local>
2025-04-16 12:18:21 +0200

    [CODE] changes to activity df, new: duration column

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 
 def main():
-    st.title("TST Finance Calculator")
-    
+    st.title("TST :blue[Finance] Calculator")
     # Project type selection dropdown
     project_type = st.selectbox(
         "Select Project Type",
@@ -13,16 +12,16 @@ def main():
     # project_type = 'TAS# TechCom 2025'
     
     # File uploader
-    uploaded_file = st.file_uploader("Upload Techcom Time Sheet (Excel)", type=["xlsx", "xls"])
+    uploaded_file = st.file_uploader("Time Sheet (Excel)", type=["xlsx", "xls"])
 
     # File uploader 2
-    uploaded_file_2 = st.file_uploader("Upload Employee/Activity Rate Sheet (Excel)", type=["xlsx", "xls"])
-    
+    uploaded_file_2 = st.file_uploader("Employee/Activity Rate Sheet (Excel)", type=["xlsx", "xls"])
+    st.divider()  
     if uploaded_file and uploaded_file_2 is not None:
         try:
             # Show success message
             st.success(f"Files uploaded successfully!")
-
+            # st.snow()
             # Read the Excel file
             df = pd.read_excel(uploaded_file)
             employee_rates_df = pd.read_excel(uploaded_file_2)
@@ -30,7 +29,7 @@ def main():
 
             df = df[df['Project description']== project_type] 
 
-            # Display Hours per Activity for each Employee
+            # Groupby Hours per Activity for each Employee
             employee_activity_hours_df = df.groupby(['Employees', 'Activity'])['Duration'].sum().reset_index()
 
             # First, merge employee_activities with activity_rates
@@ -47,29 +46,39 @@ def main():
                 if row['Activity']=='ONC' or row['Activity']=='210015_OnCall_BD(22-08:00orSat)':
                     row['Total cost'] = row['Duration'] * row['Rate in percentage']
 
-            # Group by Activity
-            act_array, act_tot_array = [],[]
+            # Group by types of Activity and compute total cost and duration
+            act_array, act_tot_array, act_dur_array = [],[], []
             activities = employee_activity_hours_df['Activity'].unique()
+            
             for activity in activities:
                 activity_total = employee_activity_hours_df[employee_activity_hours_df['Activity']==activity]['Total cost'].sum()
+                activity_total_duration = employee_activity_hours_df[employee_activity_hours_df['Activity']==activity]['Duration'].sum()
                 act_array.append(activity)
+                act_dur_array.append(activity_total_duration)
                 act_tot_array.append(activity_total) 
             activity_data = {
-                    "Activity": act_array, "Total Cost": act_tot_array
+                    "Activity": act_array, "Duration": act_dur_array, "Total Cost": act_tot_array
             }
-            activity_df = pd.DataFrame(activity_data) 
-            st.subheader("Costs Per Activity")
-            st.write(activity_df)
-            st.subheader("Total Costs for all activities is "+str(activity_df['Total Cost'].sum()))
+            activity_df = pd.DataFrame(activity_data)
+            # Display 
+            st.subheader("Duration and Cost Per :blue[Activity]", divider="gray")
+            st.write("No.of Activities: ", len(activities))
+            st.write("Total Cost: ", round(activity_df['Total Cost'].sum(),2))
+            activity_df
+            
 
             # Group by Employee and create separate dataframes with subtotals 
             employees = employee_activity_hours_df['Employees'].unique()
+            st.subheader("Duration and Cost Per :blue[Employee]", divider="gray")
+            st.write("No.of Employees: ", len(employees))
             for emp in employees: 
                 employee_df = employee_activity_hours_df[employee_activity_hours_df['Employees']==emp]
                 subtotal = employee_df['Total cost'].sum()
-                st.subheader("Costs for "+ emp +"  is "+str(subtotal))
-                st.write(employee_df)
-            
+                # Display
+                st.subheader(":blue[*"+ emp +"*] ")
+                st.write("Total Cost: ", round(subtotal,2))
+                employee_df
+
 
         except Exception as e:
             st.error(f"Error processing the file: {e}")
